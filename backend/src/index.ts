a417d27028793989ffe18bc625e845702aae5313
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2025-02-09 12:50:14 +0100

    Create auth routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes"
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
 const app = express();
+app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
   console.log(`server started ${PORT}`);
