d681cc310fdd902360db1e6bfeb6236a5112480b
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2025-02-09 17:16:46 +0100

    signup function

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,7 +1,46 @@
 import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import { PrismaClient } from "@prisma/client";
+import { handleError } from "../utils/errorHandler";
+import { generateToken } from "../utils/jwt";
 
-export const signup = (req: Request, res: Response) => {
-  res.send("signup");
+const prisma = new PrismaClient();
+
+export const signup = async (req: Request, res: Response): Promise<void> => {
+  const { fullName, email, password } = req.body;
+
+  if (!fullName || !email || !password) {
+    return handleError(res, 400, "All fields are required");
+  }
+
+  if (password.length < 6) {
+    return handleError(res, 400, "Password must be at least 6 characters");
+  }
+
+  try {
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser) {
+      return handleError(res, 400, "Email already exists");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: { fullName, email, password: hashedPassword },
+    });
+
+    // JWT generálás regisztráció utáni egybőli belépéshez
+    generateToken(Number(newUser.id), res);
+
+    res.status(201).json({
+      id: newUser.id,
+      fullName: newUser.fullName,
+      email: newUser.email,     
+    });
+  } catch (error) {
+    console.error("An error occurred during registration:", error);
+    return handleError(res, 500, "An error occurred during registration.");
+  }
 };
 
 export const login = (req: Request, res: Response) => {
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/authRoutes"
+import authRoutes from "./routes/authRoutes";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
 const app = express();
-app.use("/api/auth", authRoutes)
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`server started ${PORT}`);
