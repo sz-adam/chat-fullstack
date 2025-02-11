ea90e4f9011ce27190d7f043b3c78d2be17c097a
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2025-02-11 18:51:48 +0100

    add navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,32 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import LoginPage from "./pages/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage";
+import RootLayout from "./components/RootLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    // errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <LoginPage /> },
+      { path: "/signin", element: <RegisterPage /> },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-green-500">
-        Hello world!
-      </h1>
-    </>
+    <div
+      className="h-screen w-screen items-center justify-center bg-cover bg-center text-white"
+      style={{
+        backgroundImage:
+          "url('https://images.pexels.com/photos/462162/pexels-photo-462162.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')",
+      }}
+    >
+      <RouterProvider router={router} />;
+    </div>
   );
 }
 
---
+++ frontend/src/components/RootLayout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+import MainNavigation from "../components/MainNavigation";
+
+function RootLayout() {
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
