b9e33a3c306502168114d5a439ceb68f71975178
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2025-02-13 17:14:33 +0100

    add modell

---
+++ frontend/src/model/UserModel.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: number;
+  fullName: string;
+  email: string;
+  password: string;
+  profilePic?: string;
+  isDarkMode: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
