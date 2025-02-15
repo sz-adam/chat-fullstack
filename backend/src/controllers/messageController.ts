2605e4f9d2c207024ddc42ef7e8b279ac774bb57
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2025-02-15 19:29:49 +0100

    send a message

--- backend/src/controllers/messageController.ts
+++ backend/src/controllers/messageController.ts
@@ -36,3 +36,28 @@ export const allUser = async (
     handleError(res, 500, "Something went wrong");
   }
 };
+
+export const sendMessage = async (
+  req: AuthenticatedRequest,
+  res: Response
+): Promise<void> => {
+  try {
+    const { text } = req.body;
+    const receiverId = Number(req.params?.id);
+    const senderId = Number(req.user?.id);
+
+    const newMessage = await prisma.message.create({
+      data: {
+        senderId,
+        receiverId,
+        text,
+        createdAt: new Date(),
+      },
+    });
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.error("Error in sendMessage controller:", error);
+    handleError(res, 500, "Internal server error");
+  }
+};
--- backend/src/routes/messageRoutes.ts
+++ backend/src/routes/messageRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { allUser } from "../controllers/messageController";
+import { allUser, sendMessage } from "../controllers/messageController";
 import { protectRoute } from "../middleware/authMiddleware";
 
 
 const router = express.Router();
 
 router.get("/users", protectRoute, allUser);
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
