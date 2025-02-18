48e806d2a24a99ba0788249679d35fa6fd278dc5
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2025-02-18 11:04:23 +0100

    display date of sending

--- frontend/src/components/ChatWindow.tsx
+++ frontend/src/components/ChatWindow.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useMessages } from "../context/MessagesContext";
 import { useAuth } from "../context/AuthContext";
 
 const ChatWindow = () => {
   const { receiverMessages } = useMessages();
   const { authUser } = useAuth();
+  const [selectedMessageId, setSelectedMessageId] = useState<number | null>(
+    null
+  );
 
   //  Időrendbe való rendezés
   const sortedMessages = [...receiverMessages].sort(
@@ -16,13 +19,19 @@ const ChatWindow = () => {
       <div className="flex-1 overflow-y-auto space-y-4">
         {sortedMessages.map((msg) => (
           <div
+            onClick={() => setSelectedMessageId(msg.id)}
             key={msg.id}
             className={`chat ${
               msg.senderId === authUser!.id ? "chat-end" : "chat-start"
             }`}
           >
-            <div className="chat-bubble max-w-[70%]">
+            <div className="chat-bubble max-w-[70%] cursor-pointer">
               <p>{msg.text}</p>
+              {selectedMessageId === msg.id && (
+                <span className="text-xs text-gray-500 block mt-1">
+                  {new Date(msg.createdAt).toLocaleString()}
+                </span>
+              )}
             </div>
           </div>
         ))}
