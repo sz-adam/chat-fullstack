59608d534ec5f5dccf5559cb9e6455d1d73fd071
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2025-03-05 23:29:35 +0100

    renamed from login button ,use the same button for login and registration

--- mobile_flutter/lib/widgets/auth_button.dart
+++ mobile_flutter/lib/widgets/auth_button.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
-class LoginButton extends StatelessWidget {
+class AuthButton extends StatelessWidget {
   final bool isLoggedIn;
   final VoidCallback onPressed;
+  final String buttonText;
 
-  LoginButton({required this.isLoggedIn, required this.onPressed});
+  AuthButton(
+      {required this.isLoggedIn,
+      required this.onPressed,
+      required this.buttonText});
 //* Átalakítani a regisztrációhóz*/
   @override
   Widget build(BuildContext context) {
@@ -13,19 +17,26 @@ class LoginButton extends StatelessWidget {
       width: isLoggedIn ? 50 : 140,
       height: 50,
       decoration: BoxDecoration(
-        gradient:
-            const LinearGradient(colors: [Colors.greenAccent, Colors.teal]),
+        gradient: const LinearGradient(
+          colors: [Colors.greenAccent, Colors.teal],
+        ),
         borderRadius: BorderRadius.circular(isLoggedIn ? 50 : 20),
         boxShadow: const [
-          BoxShadow(color: Colors.black26, blurRadius: 8, offset: Offset(2, 4)),
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 8,
+            offset: Offset(2, 4),
+          ),
         ],
       ),
       child: isLoggedIn
           ? const Center(child: Icon(Icons.check, color: Colors.white))
           : TextButton(
               onPressed: onPressed,
-              child: const Text("Login",
-                  style: TextStyle(color: Colors.white, fontSize: 18)),
+              child: Text(
+                buttonText,
+                style: const TextStyle(color: Colors.white, fontSize: 18),
+              ),
             ),
     );
   }
